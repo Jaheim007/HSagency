c71377b99707287f7ce129a235257b76108d2a80
iMac-13 <imac-13@192.168.50.13>
2022-05-14 16:23:15 +0000

    everything changed

--- Front/views.py
+++ Front/views.py
@@ -2,35 +2,18 @@ from email.mime import image
 from django.shortcuts import render
 from . import models
 
-def index(request):   
-    slide = models.Slider.objects.filter().first()
-    return render(request, 'index.html',locals() )  
-
-def about(request):   
-    return render(request, 'EstateAgency/EstateAgency/about.html')  
-
-def agent_single(request):   
-    return render(request, 'EstateAgency/EstateAgency/agents-single.html')  
-
-def agents_grid(request):   
-    return render(request, 'EstateAgency/EstateAgency/agents-grid.html')  
-
-def blog_single(request):   
-    return render(request, 'EstateAgency/EstateAgency/blog-single.html')  
-
-def blog_grid(request):   
-    return render(request, 'EstateAgency/EstateAgency/blog-grid.html')  
-
-def contact(request):   
-    return render(request, 'EstateAgency/EstateAgency/contact.html')  
-
-def property_grid(request):   
-    return render(request, 'EstateAgency/EstateAgency/property-grid.html')  
-
-def property_single(request):   
-    return render(request, 'EstateAgency/EstateAgency/property-single.html')  
- 
+def index(request):
+    
+    return render(request,'pages/index.html')
     
 
+def about(request):
+    
+    return render(request,'pages/about.html')
+
+
+def property(request):
+    
+    return render(request,'pages/property.html')
 
 # Create your views here.
