e5f70efe19aea91ec8b34d8c3ef08b2b58123549
iMac-13 <imac-13@192.168.50.16>
2022-05-14 13:54:26 +0000

    delete files

--- Front/views.py
+++ Front/views.py
@@ -1,8 +1,10 @@
+from email.mime import image
 from django.shortcuts import render
-from .models import *
+from . import models
 
 def index(request):   
-    return render(request, 'EstateAgency/EstateAgency/index.html')  
+    slide = models.Slider.objects.filter().first()
+    return render(request, 'index.html',locals() )  
 
 def about(request):   
     return render(request, 'EstateAgency/EstateAgency/about.html')  
