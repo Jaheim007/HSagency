d93455d8d7bd5fb4b3824ffc25a7626ecd45fe4d
Jaheim007 <jaheimkouaho@gmail.com>
2022-05-19 14:48:34 +0000

    contact done

--- Front/urls.py
+++ Front/urls.py
@@ -2,13 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',views.index, name='home'),
-    path('agentSingle',views.agent_single, name='agentSingle'),
-    path('agentGrid',views.agents_grid, name='agentGrid'),
-    path('blog',views.blog_grid, name='blog'),
-    path('blogSingle',views.blog_single, name='blogSingle'),
-    path('property',views.property_grid, name='property'),
-    path('propertySingle',views.property_single, name='propertySingle'),
-    path('contact',views.contact, name='contact'),
-    path('about',views.about, name='about'),
+    path('',views.index,name = "index"),
+    path('about',views.about,name="about"),
+    path('property',views.property,name="property"),
+    path('contact',views.contact, name="contact")
 ]
--- Front/views.py
+++ Front/views.py
@@ -1,34 +1,23 @@
+from email.mime import image
 from django.shortcuts import render
-from .models import *
+from . import models
 
-def index(request):   
-    return render(request, 'EstateAgency/EstateAgency/index.html')  
-
-def about(request):   
-    return render(request, 'EstateAgency/EstateAgency/about.html')  
-
-def agent_single(request):   
-    return render(request, 'EstateAgency/EstateAgency/agents-single.html')  
-
-def agents_grid(request):   
-    return render(request, 'EstateAgency/EstateAgency/agents-grid.html')  
-
-def blog_single(request):   
-    return render(request, 'EstateAgency/EstateAgency/blog-single.html')  
-
-def blog_grid(request):   
-    return render(request, 'EstateAgency/EstateAgency/blog-grid.html')  
-
-def contact(request):   
-    return render(request, 'EstateAgency/EstateAgency/contact.html')  
-
-def property_grid(request):   
-    return render(request, 'EstateAgency/EstateAgency/property-grid.html')  
-
-def property_single(request):   
-    return render(request, 'EstateAgency/EstateAgency/property-single.html')  
- 
+def index(request):
+    
+    return render(request,'pages/index.html')
     
 
+def about(request):
+    
+    return render(request,'pages/about.html')
+
+
+def property(request):
+    
+    return render(request,'pages/property.html')
+
+def contact(request):
+    
+    return render(request,'pages/contact.html')
 
 # Create your views here.
