ac14465d2ace745db3f0a67430d1aba6cf1146a1
koueAnicet <68559596+koueAnicet@users.noreply.github.com>
2022-05-21 13:40:30 +0000

    urls page terminés

--- Front/urls.py
+++ Front/urls.py
@@ -4,5 +4,11 @@ from . import views
 urlpatterns = [
     path('',views.index,name = "index"),
     path('about',views.about,name="about"),
-    path('property',views.property,name="property")
+    path('property',views.property,name="property"),
+    path('property_single',views.property_single,name="property_single"),
+    path('blog',views.blog,name="blog"),
+    path('blog_single',views.blog_single,name="blog_single"),
+    path('agents', views.agents, name="agents"),
+    path('contact', views.contact, name="contact"),
+    path('agent_single', views.agent_single, name="agent_single"),
 ]
--- Front/views.py
+++ Front/views.py
@@ -3,17 +3,41 @@ from django.shortcuts import render
 from . import models
 
 def index(request):
-    
-    return render(request,'pages/index.html')
-    
-
+    home = True
+    return render(request,'pages/index.html', locals())
+#---------------views about-----------------#     
 def about(request):
-    
-    return render(request,'pages/about.html')
+    about= True
+    return render(request,'pages/about.html', locals())
 
-
+#---------------views property-----------------#  
 def property(request):
+    property = True
+    return render(request,'pages/property.html', locals())
+
+def property_single(request):
     
-    return render(request,'pages/property.html')
+    return render(request,'pages/property-single.html', locals())
+#----------------views  blog-------------------#  
+
+def blog(request):
+    blog = True
+    return render(request,'pages/blog.html', locals())
+
+def blog_single(request):
+    
+    return render(request,'pages/blog-single.html', locals())
+#---------------views agents-----------------#    
+def agents(request):
+    return render(request, "pages/agents.html")
+
+#---------------views contacts-----------------#  
+def contact(request):
+    contact = True
+    return render(request, "pages/contact.html", locals())
+
+#------------views agent-single--------------#  
+def agent_single(request):
+    return render(request, "pages/agent-single.html")
 
 # Create your views here.
