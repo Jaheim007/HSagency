650517af76468aeffeaea6e89a0d66c4200639b9
FranckyPoop <afrifranck2003@gmail.com>
2022-06-16 17:10:19 +0000

    Amélioration du fichier urls.py de l'application Front

--- Front/urls.py
+++ Front/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name = "front_index"),
-    path('about', views.about, name="front_about"),
-    path('property', views.property, name="front_property"),
-    path('property-single/<int:property_id>', views.property_single, name="front_property_single"),
+    path('about/', views.about, name="front_about"),
+    path('property/', views.property, name="front_property"),
+    path('property/<int:property_id>/', views.property_single, name="front_property_single"),
 ]
